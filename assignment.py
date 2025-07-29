af4d95240f3c89a6c95ae3716b5a26505451af8b
Retostephen <retostephen2006@gmail.com>
2025-07-29 22:51:33 +0100

    I did the assignment on creating a dictionary and it worked well

---
+++ assignment.py
@@ -0,0 +1,8 @@
+response = {
+        "Success": True,
+        "Message": "Registeration Succesfull", 
+        "Data": ["email", "username"]
+        }
+print(response["Success"])
+print(response["Message"])
+print(response["Data"])
